82877a3f492e07823b0814326e70b4b795e917a1
Regina Clifford <ginamarion.clifford@gmail.com>
2018-11-05 15:49:11 +0300

    added components to app.routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component';
+import { ContactComponent } from './contact/contact.component';
+import { MinistriesComponent } from './ministries/ministries.component';
+import { MediaComponent } from './media/media.component';
 
 const routes: Routes = [
     { path: '', component: HomepageComponent },
     { path: 'home', component: HomepageComponent},
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'location', component: MapComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'ministries', component: MinistriesComponent },
+    { path: 'media', component: MediaComponent },
 ];
 
 @NgModule({
